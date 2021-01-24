389116e26b770ef3df98a58c10745916c655c350
Janko <janko.prester@gmail.com>
2021-01-24 11:43:41 +0100

    remove unnecessary sections

--- src/pages/HomePage/Home.js
+++ src/pages/HomePage/Home.js
@@ -1,13 +1,12 @@
 import React from "react";
 import HeroSection from "../../components/HeroSection";
-import { homeObjOne, homeObjTwo, homeObjThree } from "./Data";
+import { homeObjOne, homeObjThree } from "./Data";
 //import Pricing from "../../components/Pricing";
 
 function Home() {
   return (
     <>
       <HeroSection {...homeObjOne} />
-      <HeroSection {...homeObjTwo} />
       <HeroSection {...homeObjThree} />
     </>
   );
